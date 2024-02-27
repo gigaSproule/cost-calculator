c6d3878270f752b327398690c72235120d289507
Benjamin Sproule <benjamin@benjaminsproule.com>
2024-02-27 22:09:41 +0000

    Remove cli

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
 #![windows_subsystem = "windows"]
-use std::env;
 
-use cli::load_cli;
 use ui::load_ui;
 
 mod calculator;
-mod cli;
 mod store;
 mod ui;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.contains(&String::from("--cli")) {
-        load_cli();
-    } else {
-        load_ui();
-    }
+    load_ui();
 }
