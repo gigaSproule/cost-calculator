e8a5b22ba5cf025bc6850e64e65595c2b7805abd
Benjamin Sproule <benjamin@benjaminsproule.com>
2025-01-23 22:39:47 +0000

    Latest changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.benjaminsproule.costcalculator"
         minSdk = 29
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.0.2"
+        versionCode = 7
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
